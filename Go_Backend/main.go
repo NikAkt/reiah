1437c751805f696879beb4f822dbfea386a76b9d
Eneko O'Kelly <eneko.okelly@ucdconnect.ie>
2024-06-19 13:34:48 +0200

    Initial routes/handlers for issue #33
    - Created routes on the main.go and corresponding handlers in handlers/data.go to serve json files
    
    - At the moment no data processing is done. The handlers are serving the json data to the routes
    
    - Modified the Dockerfile paths and  added a Copy for the frontend (Or files cannot be fetched). Adjusted composer to align.

--- Go_Backend/main.go
+++ Go_Backend/main.go
@@ -36,6 +36,14 @@ func main() {
 	e.GET("/register", handlers.HandleRegisterPage, middleware.Logger())
 	e.POST("/register", handlers.HandleRegisterAttempt, middleware.Logger())
 
+	// Routes
+	e.GET("/", handlers.HandleHome, handlers.CustomAuthMiddleware)
+	e.GET("/data/amenities", handlers.ServeAmenitiesData)
+	e.GET("/data/businesses", handlers.ServeBusinessData)
+	e.GET("/data/real_estate_prices", handlers.ServeRealEstatePriceData)
+	e.GET("/data/historic_real_estate_prices", handlers.ServeHistoricRealEstatePrices)
+	e.GET("/data/neighbourhoods", handlers.ServeNeighbourhoods)
+
 	// Mount the public folder at the publci address for accessing css and static files
 	e.Static("/public", "public")
 	e.Logger.Fatal(e.Start("0.0.0.0:8000"))
