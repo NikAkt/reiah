4cf99aee6d8999574ae00858a76711e5a8771e49
Eneko O'Kelly <eneko.okelly@ucdconnect.ie>
2024-06-19 13:34:48 +0200

    Initial routes/handlers for issue #33
    - Created routes on the main.go and corresponding handlers in handlers/data.go to serve json files
    
    - At the moment no data processing is done. The handlers are serving the json data to the routes
    
    - Modified the Dockerfile paths and  added a Copy for the frontend (Or files cannot be fetched). Adjusted composer to align.

---
+++ backend/handlers/data.go
@@ -0,0 +1,25 @@
+package handlers
+
+import (
+	"github.com/labstack/echo/v4"
+)
+
+func ServeAmenitiesData(c echo.Context) error {
+	return c.File("public/assets/cleaned_amenities_data2.json")
+}
+
+func ServeBusinessData(c echo.Context) error {
+	return c.File("public/assets/cleaned_business_data.json")
+}
+
+func ServeRealEstatePriceData(c echo.Context) error {
+	return c.File("public/assets/real_estate_price_data.json")
+}
+
+func ServeHistoricRealEstatePrices(c echo.Context) error {
+	return c.File("public/assets/historic_real_estate_prices.json")
+}
+
+func ServeNeighbourhoods(c echo.Context) error {
+	return c.File("public/assets/NYC_neighbourhood.geojson")
+}
