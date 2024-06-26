276ece341209c1925092d53e923452011cfd923c
Nick Aktoudianakis <nick.aktoudianakis@ucdconnect.ie>
2024-06-26 19:23:36 +0300

    Added a minimalistic landing page

--- templfrontend-rewrite/src/pages/Home.jsx
+++ templfrontend-rewrite/src/pages/Home.jsx
@@ -1,20 +1,62 @@
 import { BaseLayout } from "../layouts/Layout";
 import { A } from "@solidjs/router";
+import magnGlass from "../assets/magn_glass_transp.png";
+import appLogo from "../assets/logo_v_small.png";
 
 export const Home = () => {
   return (
     <BaseLayout>
-      <header>
-        <a
-          href="/dashboard"
-          class="flex items-center py-2 px-4 text-gray-900 hover:bg-gray-950 hover:text-gray-100 rounded-md"
-        >
-          <span class="text-lg">Go To Dashboard</span>
-        </a>
+      <header
+        class="flex justify-between items-center py-4 px-8 text-white"
+        style={{ backgroundColor: "#0FA6A2" }}
+      >
+        <div class="flex items-center space-x-2">
+          <a href="/">
+            <img
+              src={appLogo}
+              alt="App Logo"
+              style={{ width: "200px", height: "auto" }}
+            />
+          </a>
+        </div>
       </header>
-      <div class="mt-8 text-3xl">
-        <h1>Landing Page</h1>
+      <div class="flex flex-col md:flex-row items-center justify-between mt-8 mx-4 md:mx-16">
+        <div class="text-left max-w-md">
+          <h1 class="text-4xl font-bold text-teal-500">
+            Accelerate your real estate journey with Reiah
+          </h1>
+          <span class="text-sm text-teal-500 italic">Pronounced ray-ah</span>
+          <p class="mt-4 text-gray-700">
+            Jump in! Reiah helps you discover the perfect zip code to invest in,
+            whether it's for residential or commercial purposes. Our platform
+            provides the insights and tools you need to accelerate your
+            investment journey. Unlock the potential of your next real estate
+            venture with Reiah!
+          </p>
+          <div class="mt-6 flex space-x-4">
+            <a
+              href="/login"
+              class="inline-block bg-teal-500 text-white py-2 px-4 rounded-md hover:bg-teal-600 transition duration-300 ease-in-out transform hover:scale-105"
+            >
+              Log In
+            </a>
+            <a
+              href="/signup"
+              class="inline-block bg-teal-500 text-white py-2 px-4 rounded-md hover:bg-teal-600 transition duration-300 ease-in-out transform hover:scale-105"
+            >
+              Sign Up
+            </a>
+          </div>
+        </div>
+        <div class="mt-8 md:mt-0">
+          <img
+            src={magnGlass}
+            alt="magnifying glass"
+            class="w-full md:w-3/4 lg:w-1/2"
+            style={{ width: "500px", height: "auto" }}
+          />
+        </div>
       </div>
     </BaseLayout>
-  )
-}
+  );
+};
