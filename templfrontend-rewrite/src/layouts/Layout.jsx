011382f4670abbfed7323145fa360d22734cb11c
Nick Aktoudianakis <nick.aktoudianakis@ucdconnect.ie>
2024-06-26 19:23:36 +0300

    Added a minimalistic landing page

--- templfrontend-rewrite/src/layouts/Layout.jsx
+++ templfrontend-rewrite/src/layouts/Layout.jsx
@@ -1,6 +1,6 @@
 import { Sidebar, DarkLightModeToggle } from "../components/Sidebar";
 import { SortableGroup } from "../components/SortableWrapper";
-import { Navbar } from "../components/Navbar"
+import { Navbar } from "../components/Navbar";
 import { setStore, store } from "../data/stores";
 import { Show } from "solid-js";
 
@@ -8,22 +8,27 @@ const links = [
   { Href: "/map", Title: "Map" },
   { Href: "/settings", Title: "Settings" },
   { Href: "/dashboard", Title: "Dashboard" },
-]
+];
 
-
-const BaseLayout = (props) => {
+const BaseLayout = ({ children }) => {
   return (
-    <div class='text-gray-800'>
-      {props.children}
+    <div
+      class="min-h-screen bg-gray-200"
+      style={{ backgroundColor: "#F3F4F6" }}
+    >
+      {children}
     </div>
   );
-}
+};
 
 const DashboardView = (props) => {
   return (
     <BaseLayout>
       <Sidebar links={links} />
-      <main class="z-10 w-full bg-gray-100 dark:bg-gray-900 min-h-screen" onMouseOver={() => setStore({ ...store, sidebarOpen: false })}>
+      <main
+        class="z-10 w-full bg-gray-100 dark:bg-gray-900 min-h-screen"
+        onMouseOver={() => setStore({ ...store, sidebarOpen: false })}
+      >
         <Navbar />
         <Show when={props.sortableIsOn}>
           <SortableGroup class="p-8 grid grid-cols-3 gap-4">
@@ -31,29 +36,26 @@ const DashboardView = (props) => {
           </SortableGroup>
         </Show>
         <Show when={!props.sortableIsOn}>
-          <div class="p-8 grid grid-cols-3 gap-4">
-            {props.children}
-          </div>
+          <div class="p-8 grid grid-cols-3 gap-4">{props.children}</div>
         </Show>
       </main>
-    </BaseLayout >
-  )
-}
-
+    </BaseLayout>
+  );
+};
 
 const MapView = (props) => {
   return (
-    <BaseLayout >
+    <BaseLayout>
       <Sidebar links={links} />
-      <main class="z-10 w-full bg-gray-100 dark:bg-gray-900 min-h-screen" onMouseOver={() => setStore({ ...store, sidebarOpen: false })}>
-        <div>
-          {props.children}
-        </div>
+      <main
+        class="z-10 w-full bg-gray-100 dark:bg-gray-900 min-h-screen"
+        onMouseOver={() => setStore({ ...store, sidebarOpen: false })}
+      >
+        <div>{props.children}</div>
       </main>
-    </BaseLayout >
-  )
-}
-
+    </BaseLayout>
+  );
+};
 
 const AuthLayout = (props) => {
   return (
@@ -62,26 +64,21 @@ const AuthLayout = (props) => {
         <div class="h-full flex justify-center items-center bg-white dark:bg-slate-800">
           {props.children}
         </div>
-        <div
-          class="h-full bg-gradient-to-br from-teal-500 to-green-200 dark:from-teal-300 dark:to-green-950 p-48 flex justify-center items-center flex-col"
-        >
+        <div class="h-full bg-gradient-to-br from-teal-500 to-green-200 dark:from-teal-300 dark:to-green-950 p-48 flex justify-center items-center flex-col">
           <div class="fixed right-4 top-4">
             <DarkLightModeToggle />
           </div>
-          <h2 class="text-white  text-center text-3xl">Join the 2 other investors using REIAH today!!</h2>
+          <h2 class="text-white  text-center text-3xl">
+            Join the 2 other investors using REIAH today!!
+          </h2>
           <p class="text-white  text-center text-md mt-12">
-            wether you are looking for that next big investment or a place to call your own REIAH is here to guide you in the
-            right direction.
+            wether you are looking for that next big investment or a place to
+            call your own REIAH is here to guide you in the right direction.
           </p>
         </div>
       </div>
     </BaseLayout>
-  )
-}
+  );
+};
 
-export {
-  AuthLayout,
-  MapView,
-  DashboardView,
-  BaseLayout
-}
+export { AuthLayout, MapView, DashboardView, BaseLayout };
--- templfrontend-rewrite/tailwind.config.js
+++ templfrontend-rewrite/tailwind.config.js
@@ -1,34 +1,32 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  darkMode: ['selector', '[data-mode="dark"]'],
-  content: ["./src/**/*.{js,jsx,ts,tsx}",],
-  safelist: [],
-  extend: {
-    colors: {
-      'text-color': '#090e0c',
-      'background-color': '#f9fbfa',
-      'primary-color': '#10b981',
-      'secondary-color': '#bbf7d0',
-      'accent-color': '#ac9d87',
+  darkMode: ["selector", '[data-mode="dark"]'],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        "text-color": "#090e0c",
+        "background-color": "#f9fbfa",
+        "primary-color": "#10b981",
+        "secondary-color": "#bbf7d0",
+        "accent-color": "#ac9d87",
+        "custom-bg-color": "#F3F4F6",
+      },
+      animation: {
+        "fade-in": "fadeIn 2s ease-in forwards",
+        "slide-in": "slideIn 2s ease-in-out forwards",
+      },
+      keyframes: {
+        fadeIn: {
+          "0%": { opacity: "0" },
+          "100%": { opacity: "1" },
+        },
+        slideIn: {
+          "0%": { transform: "translateX(100%)" },
+          "100%": { transform: "translateX(0)" },
+        },
+      },
     },
   },
-  theme: {
-    fontSize: {
-      sm: '0.750rem',
-      base: '1rem',
-      xl: '1.333rem',
-      '2xl': '1.777rem',
-      '3xl': '2.369rem',
-      '4xl': '3.158rem',
-      '5xl': '4.210rem',
-    },
-    fontFamily: {
-      heading: 'Noto Sans Cuneiform',
-      body: 'Noto Sans Cuneiform',
-    },
-    fontWeight: {
-      normal: '400',
-      bold: '700',
-    },
-  }
-}
+  plugins: [],
+};
