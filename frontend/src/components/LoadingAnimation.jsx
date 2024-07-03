8d94a3f73632b48680b5163720848a7dad478873
CaitlinLi123 <104264689+CaitlinLi123@users.noreply.github.com>
2024-07-03 19:33:40 +0100

    feat: change the color of loading animation component

--- frontend/src/components/LoadingAnimation.jsx
+++ frontend/src/components/LoadingAnimation.jsx
@@ -1,11 +1,11 @@
 const LoadingAnimation = () => {
   return (
     <div
-      class="absolute w-screen h-screen bg-[#E4E4E7]/40 z-100 flex flex-col items-center
+      class="absolute w-screen h-screen bg-[#e6e2fd]/40 z-100 flex flex-col items-center
     "
     >
       <div
-        class="bg-[#0145ac] w-1/5 h-[1vh] absolute rounded-lg
+        class="bg-teal-500 w-1/5 h-[1vh] absolute rounded-lg
       left-0 animate-slide "
       ></div>
       <h1 class="mt-[2vh]">Loading... </h1>
