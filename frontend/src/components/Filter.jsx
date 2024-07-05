c73b3a413b17348c04fe0a83541494cffab604ec
Eneko O'Kelly <eneko.okelly@ucdconnect.ie>
2024-07-05 18:30:48 +0100

    Fixec bttom container issue on Filter
    - Fixec bottom container not staying fixed at the bottom

--- frontend/src/components/Filter.jsx
+++ frontend/src/components/Filter.jsx
@@ -14,7 +14,13 @@ const Filter = ({ realEstateData, amenitiesData }) => {
   const [avgHomeValueRange, setAvgHomeValueRange] = createSignal([0, 0]);
   const [medianHouseholdIncomeRange, setMedianHouseholdIncomeRange] = createSignal([0, 0]);
 
-  const unique_borough = ["Bronx", "Manhattan", "Queens", "Brooklyn", "Staten Island"];
+  const unique_borough = [
+    "Bronx",
+    "Manhattan",
+    "Queens",
+    "Brooklyn",
+    "Staten Island",
+  ];
 
   let avg_home_value = [];
   let median_household_income = [];
@@ -31,8 +37,14 @@ const Filter = ({ realEstateData, amenitiesData }) => {
       if (!amenities_type_desc.hasOwnProperty(arr["FACILITY_T"])) {
         amenities_type_desc[arr["FACILITY_T"]] = [];
       }
-      if (!amenities_type_desc[arr["FACILITY_T"]].includes(arr["FACILITY_DOMAIN_NAME"])) {
-        amenities_type_desc[arr["FACILITY_T"]].push(arr["FACILITY_DOMAIN_NAME"]);
+      if (
+        !amenities_type_desc[arr["FACILITY_T"]].includes(
+          arr["FACILITY_DOMAIN_NAME"]
+        )
+      ) {
+        amenities_type_desc[arr["FACILITY_T"]].push(
+          arr["FACILITY_DOMAIN_NAME"]
+        );
       }
     });
   }
@@ -152,32 +164,70 @@ const Filter = ({ realEstateData, amenitiesData }) => {
   });
 
   return (
-    <div class="absolute z-30 w-[80vw] flex flex-col items-center gap-0.5 top-[2vh] left-[10vw] justify-center text-black">
+    <div
+      class="absolute z-30 w-[80vw] flex flex-col 
+    items-center gap-0.5 top-[2vh] left-[10vw] 
+    justify-center 
+    text-black"
+    >
       <button
-        class="rounded shadow-md color-zinc-900 cursor-pointer bg-white text-base mt-4 mx-6 mb-6 leading-9 py-0 px-2 text-center"
+        class="rounded shadow-md color-zinc-900 cursor-pointer 
+        bg-white text-base mt-4 mx-6 mb-6 leading-9 py-0 px-2 text-center"
         onClick={toggleFilter}
       >
         <span>Filter</span>
       </button>
       {filterDisplay() && (
-        <div class="grid-cols-1 divide-y m-0 px-0 mt-[-2vh] w-full h-[80vh] shadow-md z-20 items-center bg-white animate-fade-down rounded-lg transition-all duration-500 overflow-y-auto relative">
+        <div
+          class="grid-cols-1 divide-y m-0 px-0 mt-[-2vh] 
+          w-full h-[80vh] shadow-md
+        z-20 items-center bg-white 
+        animate-fade-down rounded-lg transition-all duration-500 overflow-y-auto relative flex flex-col"
+        >
           {/* FILTER TITLE */}
-          <div id="filter-dropdown-title" class="items-center justify-center relative flex h-[8%] bg-black text-white w-[100%] z-30 flex-row rounded-t-lg" style="position: sticky; top: 0;">
+          <div
+            id="filter-dropdown-title"
+            class="items-center justify-center relative
+            flex h-[8%] bg-black text-white w-[100%] 
+            z-30 flex-row rounded-t-lg"
+            style="position: sticky; top: 0;"
+          >
             <button
-              class="absolute rounded-full w-[20px] h-[20px] left-[2%] hover:bg-white text-white items-center flex hover:text-black justify-center cursor-pointer"
+              class="absolute rounded-full w-[20px] h-[20px] 
+              left-[2%] hover:bg-white
+            text-white items-center flex hover:text-black
+            justify-center cursor-pointer"
               onClick={toggleFilter}
             >
-              <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" />
+              <svg
+                class="h-6 w-6"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                aria-hidden="true"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M6 18L18 6M6 6l12 12"
+                />
               </svg>
             </button>
             <p>Filters for {filterTarget()}</p>
           </div>
           {/* FILTER CONTENT */}
-          <div class="w-[100%] flex flex-col h-auto relative items-center py-4 px-[10%] gap-y-2.5 bg-white" id="filter-details-container">
+          <div
+            class="w-[100%] flex flex-col h-auto relative
+            items-center py-4 px-[10%] gap-y-2.5 bg-white"
+            id="filter-details-container"
+          >
             {/* Borough Selection */}
             <div id="borough-selection-container" class="border-solid border-2 border-indigo-600 w-full p-4 rounded-lg transition-all duration-500">
-              <label htmlFor="borough-selection" class="font-sans text-2xl font-bold text-black">Borough:</label>
+              <label htmlFor="borough-selection" class="font-sans text-2xl font-bold text-black">
+                Borough:
+              </label>
               <div class="flex flex-wrap gap-2">
                 {unique_borough.map((el) => (
                   <div key={el} class="flex items-center">
@@ -196,11 +246,17 @@ const Filter = ({ realEstateData, amenitiesData }) => {
 
             {/* Neighborhood Selection */}
             {[...selectedBoroughs()].length > 0 && (
-              <div id="neighborhood-container" class="border-solid border-2 border-indigo-600 w-full p-4 rounded-lg transition-all duration-500 ease-in-out transform opacity-100 scale-100">
-                <label htmlFor="neighborhood-selection" class="font-sans text-2xl font-bold text-black">Neighborhood:</label>
+              <div id="neighborhood-container" class="border-solid border-2 border-indigo-600 w-full p-4 rounded-lg transition-all duration-500">
+                <label htmlFor="neighborhood-selection" class="font-sans text-2xl font-bold text-black">
+                  Neighborhood:
+                </label>
                 <div class="w-full h-32 overflow-y-auto border border-gray-300 rounded-md">
                   {getNeighborhoods([...selectedBoroughs()]).map((el) => (
-                    <div key={el} class={`p-2 cursor-pointer ${selectedNeighborhoods().has(el) ? 'bg-indigo-600 text-white' : 'bg-white text-black'}`} onClick={() => handleNeighborhoodChange(el)}>
+                    <div
+                      key={el}
+                      class={`p-2 cursor-pointer ${selectedNeighborhoods().has(el) ? 'bg-indigo-600 text-white' : 'bg-white text-black'}`}
+                      onClick={() => handleNeighborhoodChange(el)}
+                    >
                       {el}
                     </div>
                   ))}
@@ -210,8 +266,10 @@ const Filter = ({ realEstateData, amenitiesData }) => {
 
             {/* Zipcode Selection */}
             {[...selectedNeighborhoods()].length > 0 && (
-              <div id="zipcode-selection-container" class="border-solid border-2 border-indigo-600 w-full p-4 rounded-lg transition-all duration-500 ease-in-out transform opacity-100 scale-100">
-                <label htmlFor="zipcode-selection" class="font-sans text-2xl font-bold text-black">ZIPCODE:</label>
+              <div id="zipcode-selection-container" class="border-solid border-2 border-indigo-600 w-full p-4 rounded-lg transition-all duration-500">
+                <label htmlFor="zipcode-selection" class="font-sans text-2xl font-bold text-black">
+                  ZIPCODE:
+                </label>
                 <div class="grid grid-cols-5 overflow-y-auto h-32">
                   <div>
                     <input type="checkbox" onChange={handleSelectAllZipCodes} />
@@ -234,16 +292,24 @@ const Filter = ({ realEstateData, amenitiesData }) => {
 
             {/* Advanced Filters Button */}
             {selectedZipCodes().size > 0 && (
-              <button class="mt-4 p-2 bg-indigo-600 text-white rounded transition-all duration-500 ease-in-out transform opacity-100 scale-100" onClick={() => setShowAdvancedFilters(!showAdvancedFilters())}>
+              <button
+                class="mt-4 p-2 bg-indigo-600 text-white rounded transition-all duration-500"
+                onClick={() => setShowAdvancedFilters(!showAdvancedFilters())}
+              >
                 {showAdvancedFilters() ? "Hide Advanced Filters" : "Show Advanced Filters"}
               </button>
             )}
 
             {/* Advanced Filters */}
             {showAdvancedFilters() && (
-              <div id="advanced-filters-container" class="w-full p-4 border-solid border-2 border-indigo-600 rounded-lg mt-4 flex flex-col transition-all duration-500 ease-in-out transform opacity-100 scale-100">
-                <div class="flex flex-col items-center justify-center border-solid border-2 border-indigo-600 p-2 rounded-lg" id="home-value-container">
-                  <p class="font-sans text-2xl font-bold text-black">Average Home Value</p>
+              <div id="advanced-filters-container" class="w-full p-4 border-solid border-2 border-indigo-600 rounded-lg mt-4 flex flex-col transition-all duration-500">
+                <div
+                  class="flex flex-col items-center justify-center border-solid border-2 border-indigo-600 p-2 rounded-lg"
+                  id="home-value-container"
+                >
+                  <p class="font-sans text-2xl font-bold text-black">
+                    Average Home Value
+                  </p>
                   <div class="flex gap-2 w-full">
                     <div class="flex flex-col w-full border-solid border-2 border-indigo-600 rounded-lg p-2">
                       <p>Minimum</p>
@@ -264,8 +330,13 @@ const Filter = ({ realEstateData, amenitiesData }) => {
                   </div>
                 </div>
 
-                <div class="flex flex-col items-center justify-center border-solid border-2 border-indigo-600 p-2 rounded-lg" id="median-income-container">
-                  <p class="font-sans text-2xl font-bold text-black">Median Household Income</p>
+                <div
+                  class="flex flex-col items-center justify-center border-solid border-2 border-indigo-600 p-2 rounded-lg"
+                  id="median-income-container"
+                >
+                  <p class="font-sans text-2xl font-bold text-black">
+                    Median Household Income
+                  </p>
                   <div class="flex gap-2 w-full">
                     <div class="flex flex-col w-full border-solid border-2 border-indigo-600 rounded-lg p-2">
                       <p>Minimum</p>
@@ -286,8 +357,13 @@ const Filter = ({ realEstateData, amenitiesData }) => {
                   </div>
                 </div>
 
-                <div id="amenities-container" class="flex flex-col items-center justify-center border-solid border-2 border-indigo-600 p-2 rounded-lg">
-                  <p class="font-sans text-2xl font-bold text-black">Amenities</p>
+                <div
+                  id="amenities-container"
+                  class="flex flex-col items-center justify-center border-solid border-2 border-indigo-600 p-2 rounded-lg"
+                >
+                  <p class="font-sans text-2xl font-bold text-black">
+                    Amenities
+                  </p>
                   <div class="grid grid-cols-2 w-full gap-2">
                     {Object.keys(amenities_type_desc).flatMap((el) =>
                       amenities_type_desc[el].map((item) => (
@@ -309,7 +385,11 @@ const Filter = ({ realEstateData, amenitiesData }) => {
           </div>
 
           {/* Apply and Clear Buttons */}
-          <div id="button-container" class="items-center justify-center flex gap-4 h-[8%] relative bg-black text-white w-[100%] z-30 border-solid border-2 border-indigo-600 rounded-b-lg p-4" style="position: sticky; bottom: 0;">
+          <div
+            id="button-container"
+            class="items-center justify-center flex gap-4 h-[8%] bg-black text-white w-[100%] z-30 p-4 mt-auto"
+            style="position: sticky; bottom: 0;"
+          >
             <button
               class="rounded-2xl z-20 cursor-pointer w-32 h-9 flex items-center justify-center gap-1.5 hover:scale-110 duration-300 active:bg-violet-700 focus:outline-none focus:ring focus:ring-violet-300"
               onClick={() => {
@@ -323,6 +403,7 @@ const Filter = ({ realEstateData, amenitiesData }) => {
             >
               Clear all
             </button>
+
             <button
               class="rounded-2xl z-20 cursor-pointer w-32 h-9 flex items-center justify-center gap-1.5 hover:scale-110 duration-300 active:bg-violet-700 focus:outline-none focus:ring focus:ring-violet-300"
               onClick={handleApplyFilter}
