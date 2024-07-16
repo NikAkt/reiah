a4ed744186cc39176aab6944688a3d36256c590e
Eneko O'Kelly <eneko.okelly@ucdconnect.ie>
2024-07-16 15:40:00 +0100

    Json to Database Migration
    For issue #77
    
    - Updated our code to use a postgres database instead of using JSON files directly for performance purposes
    - DB connection: Added retry logic in `InitDB` function.
    - Data loader functions for various datasets.
    - Error handling and logging in the data loading process
    - Opening and closing of the database connection.
    - Updated `main.go` to call `LoadData` and set up API routes
    - Organized code for clear separation between data loading and other code
    
    Still issues but seem to be from frontend having to be adjusted

--- init-user-db.sh
+++ init-user-db.sh
@@ -1,28 +1,29 @@
 #!/bin/bash
 set -e
 
-# Check if the user exists and create if it does not
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
-  DO
-  \$do\$
-  BEGIN
-     IF NOT EXISTS (
-        SELECT 
-        FROM   pg_catalog.pg_roles
-        WHERE  rolname = 'myuser') THEN
-        CREATE ROLE myuser WITH LOGIN PASSWORD 'mypassword';
-     END IF;
-  END
-  \$do\$;
+    DO
+    \$do\$
+    BEGIN
+        IF NOT EXISTS (
+            SELECT 
+            FROM   pg_catalog.pg_roles
+            WHERE  rolname = 'myuser') THEN
+            CREATE ROLE myuser WITH LOGIN PASSWORD 'mypassword';
+        END IF;
+    END
+    \$do\$;
 EOSQL
 
-# Check if the database exists and create if it does not
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
-  SELECT 'CREATE DATABASE mydatabase OWNER myuser'
-  WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'mydatabase')\gexec
+    SELECT 'CREATE DATABASE mydatabase OWNER myuser'
+    WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'mydatabase')\gexec
 EOSQL
 
-# Grant privileges to the user
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
-  GRANT ALL PRIVILEGES ON DATABASE mydatabase TO myuser;
+    GRANT ALL PRIVILEGES ON DATABASE mydatabase TO myuser;
+    \c mydatabase
+    GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO myuser;
+    GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public TO myuser;
+    GRANT ALL PRIVILEGES ON ALL FUNCTIONS IN SCHEMA public TO myuser;
 EOSQL
