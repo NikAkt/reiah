a0e3a9797e1148a766a763929dde17b5def57c69
Eneko O'Kelly <eneko.okelly@ucdconnect.ie>
2024-07-24 11:20:25 +0100

    Changed code to use new model

--- recommendation_system/app.py
+++ recommendation_system/app.py
@@ -2,33 +2,56 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 import joblib
 import pandas as pd
+import json
 from utilities.recommendation_system import get_recommendations, predict_price
 
 app = Flask(__name__)
-CORS(app) 
+CORS(app)
 
 # Load the models
 rf_model = joblib.load('models/rf_model.joblib')
 knn_model = joblib.load('models/knn_model.joblib')
 
+# Load JSON data
+with open('data/processed_zipcode_data.json', 'r') as f:
+    zipcode_data = json.load(f)
+
 @app.route('/predict_price', methods=['POST', 'GET'])
 def predict_price_endpoint():
-    if request.method == 'POST':
-        data = request.get_json()
-    else:
-        data = request.args
-    
-    borough = data.get('borough')
-    house_type = data.get('house_type')
-    bedrooms = int(data.get('bedrooms'))
-    bathrooms = int(data.get('bathrooms'))
-    sqft = int(data.get('sqft'))
-    latitude = float(data.get('latitude'))
-    longitude = float(data.get('longitude'))
-    zipcode = data.get('zipcode')
-    
-    prediction = predict_price(borough, house_type, bedrooms, bathrooms, sqft, latitude, longitude, zipcode)
-    return jsonify({'predicted_price': prediction})
+    try:
+        if request.method == 'POST':
+            data = request.get_json()
+        else:
+            data = request.args
+
+        borough = data.get('borough')
+        house_type = data.get('house_type')
+        bedrooms = int(data.get('bedrooms'))
+        bathrooms = int(data.get('bathrooms'))
+        sqft = int(data.get('sqft'))
+        latitude = float(data.get('latitude'))
+        longitude = float(data.get('longitude'))
+        zipcode = data.get('zipcode')
+
+        if zipcode not in zipcode_data:
+            return jsonify({'error': 'Zipcode not found'}), 404
+
+        # Retrieve additional parameters based on the provided zipcode
+        zip_info = zipcode_data[zipcode]
+        population = zip_info['Population']
+        liveliness_score = zip_info['LivelinessScore']
+        median_household_income = zip_info['MedianHouseholdIncome']
+        population_density = zip_info['PopulationDensity']
+
+        prediction = predict_price(
+            borough, house_type, bedrooms, bathrooms, sqft, latitude, longitude, zipcode,
+            population, liveliness_score, median_household_income, population_density
+        )
+
+        return jsonify({'predicted_price': float(prediction)})
+    except Exception as e:
+        app.logger.error(f"Exception on /predict_price: {e}")
+        return jsonify({'error': 'An error occurred', 'message': str(e)}), 500
 
 @app.route('/get_recommendations', methods=['POST', 'GET'])
 def get_recommendations_endpoint():
